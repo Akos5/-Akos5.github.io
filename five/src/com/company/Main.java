ff523143a56c1f21c3afab99bdebac4d7653cc94
Szakcsi Ákos / R.Ü <69165982+Akos5@users.noreply.github.com>
2020-11-19 14:55:00 +0100

    Add files via upload

---
+++ five/src/com/company/Main.java
@@ -0,0 +1,56 @@
+package com.company;
+
+import sun.security.rsa.RSAUtil;
+
+import java.sql.Array;
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("Hello Akos");
+
+
+
+       /* Lipem, lopom a szőlőt,
+        elaludt az öreg csősz.
+        Furkósbot a kezébe,
+                vaskalap a fejébe. helycsere
+*/
+                // Írasd ki a "Lipem lopom a szőlőt" című mondóka sorait helyes sorrendben
+              //  System.out.println("elaludt az öreg csősz.");
+            //    System.out.println("Lipem, lopom a szőlőt,");
+          //      System.out.println("vaskalap a fejébe.");
+        //        System.out.println("Furkósbot a kezébe,");
+/*
+        int day = 2;
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+        }
+// Outputs "Thursday" (day 4)
+        System.out.println(day);
+*/
+
+    }
+}
---
+++ five/src/com/company/unit.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class unit {
+
+    public static void main(String[] args) {
+        // write your code here
+
+
+    }
+}
+
